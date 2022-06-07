75641a14c0cbebee92bed342d2386bad8d83f13b
Pickle-Morty <avatarbliz@gmail.com>
2022-06-07 22:38:51 +0600

    useEffect example

--- src/App.js
+++ src/App.js
@@ -2,34 +2,37 @@ import "./App.css";
 import Toastify from "./components/Toastify/Toastify";
 import MyRoutes from "./MyRoutes";
 import AuthContextProvider from "./components/context/AuthContextProvider";
-import { getNode, updateNode } from "./Firebase";
+import {getNode, updateNode} from "./Firebase";
+import {useEffect} from "react";
 
 function App() {
-  let hernya = updateNode(
-    "/users/",
-    {
-      username: "pasha",
-      password: "123456",
-      delete_mode: true,
-    }
-    // "-N3yjhvtaHu3Gxg4OuJr"
-  ).then((result) => {
-    console.log(result);
-  });
+    // let hernya = updateNode(
+    //     "/users/",
+    //     {
+    //         username: "pasha",
+    //         password: "123456",
+    //         delete_mode: true,
+    //     }
+    //     // "-N3yjhvtaHu3Gxg4OuJr"
+    // ).then((result) => {
+    //     console.log(result);
+    // });
 
-  let user = getNode("users").then((result) => {
-    console.log(result["-N3yjhvtaHu3Gxg4OuJr"]);
-    return result;
-  });
 
-  return (
-    <div className="App">
-      {/* <AuthContextProvider> */}
-      <Toastify />
-      <MyRoutes />
-      {/* </AuthContextProvider> */}
-    </div>
-  );
+    // useEffect(() => {
+    //     getNode("users").then((result) => {
+    //         console.log(result["-N3yjhvtaHu3Gxg4OuJr"]);
+    //     });
+    // }, [])
+
+    return (
+        <div className="App">
+            {/* <AuthContextProvider> */}
+            <Toastify />
+            <MyRoutes />
+            {/* </AuthContextProvider> */}
+        </div>
+    );
 }
 
 export default App;
--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Route, Routes } from "react-router-dom";
 import Add from "./components/admin/Add";
 import Form from "./components/Form/Form";
@@ -7,8 +7,23 @@ import MainLayout from "./layout/MainLayout";
 import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
 import Specialist from "./pages/Specialist/Specialist";
+import {getNode} from "./Firebase";
+
 
 const MyRoutes = () => {
+// Данные прокидываються
+    // 1. через props !!!!!!
+    // 2. через context
+    // 3. создаешь store и импортируешь из 
+
+    const [dataTable, setDataTable ] = useState(false)
+    
+    useEffect(() => {
+        getNode("users").then((result) => {
+            setDataTable( result)
+            console.log(result);
+        });
+    }, [])
   return (
     <Routes>
       <Route element={<MainLayout />}>
@@ -17,7 +32,7 @@ const MyRoutes = () => {
 
         <Route path="/add" element={<Add />} />
         <Route path="/form" element={<Form />} />
-        <Route path="/list" element={<List />} />
+        <Route path="/list" element={<List dataTable = {dataTable} />} />
         {/* <Route path="/register" element={<Register />} /> */}
       </Route>
     </Routes>
